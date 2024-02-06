98e74243ca311fa42d48d7bc1753bf3882671188
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-06 01:29:53 -0800

    8.6: Adding a book

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v4: uuid } = require('uuid');
 
 let authors = [
     {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]
     allAuthors: [Author]
   }
+
+  type Mutation {
+  addBook(
+    title: String!
+    published: Int
+    author: String!
+    genres: [String]!
+  ): Book
+}
 `
 
 const resolvers = {
@@ -137,6 +147,14 @@ const resolvers = {
         allAuthors: () => authors.map(author => {
             return { ...author, bookCount: books.filter(book => book.author === author.name).length }
         })
+    },
+    Mutation: {
+        addBook: (root, args) => {
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+            console.log("🚀 ~ books:", books)
+            return book
+        }
     }
 }
 
