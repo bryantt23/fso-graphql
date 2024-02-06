f3eef0dedb132b50c1d35c9f9679670232b8582b
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-06 01:07:18 -0800

    8.3: All authors

--- index.js
+++ index.js
@@ -106,10 +106,18 @@ const typeDefs = `
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: String!
+    born: Int
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks: [Book]
+    allAuthors: [Author]
   }
 `
 
@@ -117,7 +125,10 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books
+        allBooks: () => books,
+        allAuthors: () => authors.map(author => {
+            return { ...author, bookCount: books.filter(book => book.author === author.name).length }
+        })
     }
 }
 
